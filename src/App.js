dc26f0fbaba7d7eb1b66771200337eecb591722b
Carlos Kazuo <bld_cishizaka@uolinc.com>
2018-04-24 17:05:15 -0300

    Added code-splitting example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import Loadable from 'react-loadable';
 import logo from './logo.svg';
 import './App.css';
+
+const AsyncComponent = Loadable({
+  loader: () => import('./SomeComponent'),
+  loading: () => <div>loading...</div>
+});
 
 class App extends Component {
   render() {
@@ -10,9 +16,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AsyncComponent />
       </div>
     );
   }
